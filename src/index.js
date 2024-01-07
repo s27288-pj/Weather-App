ef66d98ea46eff931bd5fa84e44796506ac6b9b5
Mateusz Lisewski <s27288@pjwstk.edu.pl>
2024-01-07 20:26:04 +0100

    Update of Website

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
-let isDarkMode = false;
+// TODO: Current Weather background change depending on weather only on default mode, Light mode and Dark mode background doesn't change
+// TODO: Slider for Modes: Light, Default, Dark
+// TODO: Developer Mode for OpenWeatherMap API Key
+// TODO: 
 
-function toggleMode() {
-  const body = document.body;
-  body.classList.toggle('dark-mode');
-  isDarkMode = !isDarkMode;
-
-  // Save user preference to localStorage
-  localStorage.setItem('darkMode', isDarkMode);
-}
-
-// Check user preference in localStorage on page load
-window.onload = function () {
-  const userPrefersDark = localStorage.getItem('darkMode') === 'true';
-
-  if (userPrefersDark) {
-    toggleMode();
-  }
-};
+// TODO: Change before commit
 
 const apiKey = process.env.REACT_APP_OPENWEATHERMAPAPI;
 const mapboxToken = process.env.REACT_APP_MAPBOXTOKEN;
+
+const modeSlider = document.getElementById('modeSlider');
+const modeText = document.getElementById('modeText');
+const MODE_KEY = 'websiteMode';
+
+function updateMode(mode) {
+  switch (mode) {
+    case 'Light':
+      document.body.classList.remove('dark-mode');
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('default-mode');
+      break;
+    case 'Default':
+      document.body.classList.remove('dark-mode', 'light-mode');
+      document.body.classList.add('default-mode');
+      break;
+    case 'Dark':
+      document.body.classList.remove('light-mode');
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('default-mode');
+      break;
+    default:
+      break;
+  }
+  modeText.textContent = mode;
+  localStorage.setItem(MODE_KEY, mode);
+}
+
+if (localStorage.getItem(MODE_KEY)) {
+  const savedMode = localStorage.getItem(MODE_KEY);
+  modeSlider.value = savedMode === 'Light' ? '1' : savedMode === 'Dark' ? '3' : '2';
+  updateMode(savedMode);
+} else {
+  updateMode('Default'); // Set default mode if no saved mode is found
+}
 
 // Function to fetch location suggestions from MapBox API
 async function getLocationSuggestions(query) {
@@ -36,6 +58,7 @@ async function getLocationSuggestions(query) {
 // Function to display location suggestions
 function displayLocationSuggestions(suggestions) {
   const suggestionsList = document.getElementById('suggestions-list');
+  suggestionsList.style.display = 'block';
   suggestionsList.innerHTML = '';
 
   suggestions.forEach(suggestion => {
@@ -47,23 +70,30 @@ function displayLocationSuggestions(suggestions) {
 }
 
 // Function to handle the selection of a location from suggestions
-async function selectLocation(coordinates, placeName) {
+async function selectLocation(coordinates, place_name) {
   const [longitude, latitude] = coordinates; // Correctly extract latitude and longitude
 
   try {
     const weatherData = await getWeatherData(latitude, longitude);
     const forecastData = await getForecastData(latitude, longitude);
   
-    displayCurrentWeather(weatherData);
+    displayCurrentWeather(weatherData, place_name);
     displayForecastData(forecastData);
 
     // Set selected suggestion to the input box
-    const locationInput = document.getElementById('location-input');
-    locationInput.value = `${placeName}`;
+    const locationInput = document.getElementById('location-input-field');
+    locationInput.value = `${place_name}`;
 
     // Clear suggestions list from the DOM
     const suggestionsList = document.getElementById('suggestions-list');
     suggestionsList.innerHTML = '';
+    
+    // Hide the suggestions list
+    suggestionsList.style.display = 'none';
+
+    // Move cursor out of the input field
+    locationInput.blur(); 
+
   } catch (error) {
     console.error('Error fetching weather data:', error);
     // Handle error - for example, display an error message to the user
@@ -72,7 +102,7 @@ async function selectLocation(coordinates, placeName) {
 
 // Function to handle user input for location autocomplete
 async function handleInput() {
-  const locationInput = document.getElementById('location-input').value;
+  const locationInput = document.getElementById('location-input-field').value;
   if (locationInput.length > 0) {
     const suggestions = await getLocationSuggestions(locationInput);
     displayLocationSuggestions(suggestions);
@@ -97,18 +127,19 @@ async function getForecastData(latitude, longitude) {
 }
 
 // Function to display current weather
-function displayCurrentWeather(weather) {
+function displayCurrentWeather(weather, place_name) {
   const currentWeatherElement = document.getElementById('current-weather');
   currentWeatherElement.innerHTML = `
+    <h3>Current Weather in ${place_name}</h3>
     <p>Temperature: ${weather.main.temp}°C</p>
     <p>Feels Like: ${weather.main.feels_like}°C</p>
     <p>Pressure: ${weather.main.pressure} hPa</p>
     <p>Humidity: ${weather.main.humidity}%</p>
   `;
 
-  // Display the current weather title
-  const currentWeatherTitle = document.getElementById('current-weather-title');
-  currentWeatherTitle.style.display = 'block';
+  // Display the current weather element
+  const currentWeather = document.getElementById('current-weather');
+  currentWeather.style.display = 'block';
 }
 
 // Function to display forecast data in a table
@@ -142,12 +173,22 @@ function displayForecastData(forecast) {
   const forecastTitle = document.getElementById('forecast-title');
   const forecastTable = document.getElementById('forecast-table');
   forecastTitle.style.display = 'block';
-  forecastTable.style.display = 'table';
+  forecastTable.style.display = 'none';
+}
+
+// Function to handle the toggle of forecast table visibility
+function toggleForecastTable() {
+  const forecastTable = document.getElementById('forecast-table');
+  if (forecastTable.style.display === 'none') {
+    forecastTable.style.display = 'table';
+  } else {
+    forecastTable.style.display = 'none';
+  }
 }
 
 // Function to handle the search and retrieve weather data
 async function searchWeather() {
-  const locationInput = document.getElementById('location-input').value;
+  const locationInput = document.getElementById('location-input-field').value;
   
   // Fetch coordinates using MapBox geocoding API
   const mapboxResponse = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${locationInput}.json?access_token=${mapboxToken}`);
@@ -160,13 +201,13 @@ async function searchWeather() {
   const weatherData = await getWeatherData(`${latitude},${longitude}`);
   const forecastData = await getForecastData(`${latitude},${longitude}`);
   
-  displayCurrentWeather(weatherData);
+  displayCurrentWeather(weatherData, mapboxData.features[0].place_name);
   displayForecastData(forecastData);
 }
 
 // Function to handle search when button is clicked or Enter is pressed
 function searchWeatherOnClick() {
-  const locationInput = document.getElementById('location-input').value;
+  const locationInput = document.getElementById('location-input-field').value;
 
   if (locationInput.length > 0) {
     // Fetch suggestions from MapBox API
@@ -174,8 +215,8 @@ function searchWeatherOnClick() {
       .then(suggestions => {
         if (suggestions.length > 0) {
           const coordinates = suggestions[0].coordinates;
-          const placeName = suggestions[0].place_name;
-          selectLocation(coordinates, placeName);
+          const place_name = suggestions[0].place_name;
+          selectLocation(coordinates, place_name);
         }
       })
       .catch(error => {
@@ -183,6 +224,14 @@ function searchWeatherOnClick() {
         // Handle error - for example, display an error message to the user
       });
   }
+  else {
+    alert('Please enter a location to search for weather data.');
+  }
+}
+
+function clearInputField() {
+  const locationInput = document.getElementById('location-input-field');
+  locationInput.value = ''; // Clear the input field value
 }
 
 function displayPopupOnDesktop() {
@@ -200,8 +249,7 @@ function closePopup() {
 }
 
 // Set timeout to display the popup after 20 seconds for desktop devices
-setTimeout(displayPopupOnDesktop, 5000); // 20 seconds in milliseconds 
-// TODO: Change to 20 seconds
+setTimeout(displayPopupOnDesktop, 20000); // 20 seconds in milliseconds 
 
 const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
 let konamiCodeIndex = 0;
@@ -224,11 +272,22 @@ function checkKonamiCode(event) {
 }
 
 // Attach an event listener to the input field for real-time suggestions
-const inputField = document.getElementById('location-input');
+const inputField = document.getElementById('location-input-field');
 inputField.addEventListener('input', handleInput);
 
-const searchButton = document.querySelector('button');
+const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', searchWeatherOnClick);
+
+const locationInput = document.getElementById('location-input-field');
+locationInput.addEventListener('click', clearInputField);
+
+const forecastTitle = document.getElementById('forecast-title');
+forecastTitle.addEventListener('click', toggleForecastTable);
+
+const weatherAppIcon = document.getElementById('weather-app-icon');
+weatherAppIcon.addEventListener('click', () => {
+  location.reload();
+});
 
 // Handle Enter key press
 inputField.addEventListener('keypress', function(event) {
@@ -244,4 +303,25 @@ document.addEventListener('keydown', function(event) {
   } else {
     checkKonamiCode(event);
   }
+});
+
+// Event listener for mode slider change
+modeSlider.addEventListener('input', () => {
+  let mode = 'Default';
+
+  switch (parseInt(modeSlider.value)) {
+    case 1:
+      mode = 'Light';
+      break;
+    case 2:
+      mode = 'Default';
+      break;
+    case 3:
+      mode = 'Dark';
+      break;
+    default:
+      break;
+  }
+
+  updateMode(mode);
 });
